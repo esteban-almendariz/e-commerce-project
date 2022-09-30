970459f8718689e270a537064e7b437c448db53b
esteban-almendariz <98506796+esteban-almendariz@users.noreply.github.com>
2022-09-30 00:36:25 -0500

    added our sign in component as well as firebase utilities and brought in firebase libraries

--- src/App.js
+++ src/App.js
@@ -1,46 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react';
-import CategoryItem from './components/category-item/CategoryItem';
-import CategoryMenu from './components/category-item/CategoryMenu';
+import { Routes, Route } from "react-router-dom"
+import Home from "./routes/home/Home"
+import Navigation from './routes/navigation/Navigation';
+import SignIn from './routes/sign-in/SignIn';
 
 
 const App = () => {
 
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-
+  const Shop = () => {
+    return (
+      <h1>I am the Shop Page</h1>
+    )
+  }
 
   return (
-    <div>
-      <CategoryMenu
-        category={categories}
-      />
-    </div>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
+      </Route>
+    </Routes>
   )
 }
 
